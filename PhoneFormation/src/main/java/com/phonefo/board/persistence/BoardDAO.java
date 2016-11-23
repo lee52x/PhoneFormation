cd6829a9fb5c7c31ec5b70f3704e8d78689463b9
jaehyun <jaehyun@192.168.0.134>
2016-11-23 15:43:59 +0900

    board

--- PhoneFormation/src/main/java/com/phonefo/board/persistence/BoardDAO.java
+++ PhoneFormation/src/main/java/com/phonefo/board/persistence/BoardDAO.java
@@ -1,5 +1,12 @@
 package com.phonefo.board.persistence;
 
-public interface BoardDAO {
+import java.util.List;
 
+import com.phonefo.admin.domain.SearchCriteria;
+import com.phonefo.board.domain.BoardVO;
+
+
+public interface BoardDAO {
+	   //검색어에 대한 리스트 조회
+	   public List<BoardVO> listSearch(SearchCriteria cri)throws Exception; 
 }
--- PhoneFormation/src/main/java/com/phonefo/board/persistence/BoardDAOImpl.java
+++ PhoneFormation/src/main/java/com/phonefo/board/persistence/BoardDAOImpl.java
@@ -3,12 +3,25 @@
  */
 package com.phonefo.board.persistence;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+
+import com.phonefo.admin.domain.SearchCriteria;
+import com.phonefo.board.domain.BoardVO;
 import com.phonefo.board.persistence.BoardDAO;
 
-/**
- * @author jaehyun
- *
- */
 public class BoardDAOImpl implements BoardDAO {
+	@Inject
+	private SqlSession sqlSession;	
+	
+	@Override
+	public List<BoardVO> listSearch(SearchCriteria cri) throws Exception {
+		return sqlSession.selectList("board.listSearch",cri,
+		        new RowBounds(cri.getPageStart(), cri.getPerPageNum()) );
+	}
 
 }
